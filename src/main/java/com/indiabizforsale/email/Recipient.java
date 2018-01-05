a58837f3e468c18b67fa50e8c57814625b01201e
Devansh <devansh@indiabizforsale.com>
2018-01-05 14:35:26 +0530

    Removed code smell

--- src/main/java/com/indiabizforsale/email/Recipient.java
+++ src/main/java/com/indiabizforsale/email/Recipient.java
@@ -47,7 +47,7 @@ public class Recipient {
     public String getEmail() {
         if (getTemplateData().containsKey("name")) {
             String mail = getTemplateValue("name") + " <" + getRawEmail() + ">";
-            logger.info(mail.concat(" is the new email format."));
+            logger.info("Email format : {}", mail);
             return mail;
         } else
             return getRawEmail();
