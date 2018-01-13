ccd76398b50cf7fa8715039fc027f407d234a8a9
bala <bala@indiabizforsale.com>
2018-01-13 16:52:22 +0530

    added model classes in new package

---
+++ src/main/java/com/indiabizforsale/email/model/PayLoad.java
@@ -0,0 +1,121 @@
+package com.indiabizforsale.email.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+
+
+public class PayLoad {
+
+    private ArrayList<Recipient> to;
+    private String from;
+    private String templateId;
+    private String fromName;
+    private String bodyHtml;
+    private String bodyText;
+    private String subject;
+
+
+    public String getBodyHtml() {
+        return bodyHtml;
+    }
+
+    public void setBodyHtml(String bodyHtml) {
+        this.bodyHtml = bodyHtml;
+    }
+
+    public String getBodyText() {
+        return bodyText;
+    }
+
+    public void setBodyText(String bodyText) {
+        this.bodyText = bodyText;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+
+    /**
+     * <h1>To get template data for a single recipient</h1>
+     *
+     * @return JSon string
+     * @throws JsonProcessingException
+     */
+    public String getFirstTemplate() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(this.getTo().get(0).getTemplateData());
+
+    }
+
+    public String getFirstEmail() {
+        return this.getTo().get(0).getEmail();
+    }
+
+
+    public int getToAddressCount() {
+        int count;
+        count = this.getTo().size();
+        return count;
+    }
+
+    public ArrayList<Recipient> getTo() {
+        return to;
+    }
+
+    public void setTo(ArrayList<Recipient> to) {
+        this.to = to;
+    }
+
+    /**
+     * <h1>To return the email in the form " name of sender <email> " .</h1>
+     *
+     * @return email
+     */
+    public String getFrom() {
+        return fromName + "<" + from + ">";
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTemplateId() {
+        return templateId;
+    }
+
+    public void setTemplateId(String templateId) {
+        this.templateId = templateId;
+    }
+
+    public String getFromName() {
+        return fromName;
+    }
+
+    public void setFromName(String fromName) {
+        this.fromName = fromName;
+    }
+
+    public String getRawFrom() {
+        return from;
+    }
+
+    @Override
+    public String toString() {
+        return "PayLoad{" +
+                "to=" + to +
+                ", from='" + from + '\'' +
+                ", templateId='" + templateId + '\'' +
+                ", fromName='" + fromName + '\'' +
+                ", bodyHtml='" + bodyHtml + '\'' +
+                ", bodyText='" + bodyText + '\'' +
+                ", subject='" + subject + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/indiabizforsale/email/model/Recipient.java
@@ -0,0 +1,78 @@
+package com.indiabizforsale.email.model;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class Recipient {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Recipient.class);
+    private String email;
+    private Map<String, String> templateData;
+
+
+    public Map<String, String> getTemplateData() {
+        return templateData;
+    }
+
+    public void setTemplateData(Map<String, String> templateData) {
+        this.templateData = templateData;
+    }
+
+    /**
+     * <h1>To convert the template data of Map into JSon.</h1>
+     *
+     * @return JSon
+     * @throws JsonProcessingException
+     */
+    public String getTemplateDataJson() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+
+        return objectMapper.writeValueAsString(templateData);
+    }
+
+    /**
+     * <p>
+     * This method checks if the name of the recipient is present or not in the template data.
+     * If present then the email is sent along with the name in th form name <email> otherwise
+     * the email is returned.
+     * </p>
+     *
+     * @return email along with name ,if available.
+     */
+    public String getEmail() {
+        if (getTemplateData().containsKey("name")) {
+            String mail = getTemplateValue("name") + " <" + getRawEmail() + ">";
+            logger.info("Email format : {}", mail);
+            return mail;
+        } else
+            return getRawEmail();
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRawEmail() {
+        return email;
+    }
+
+    private String getTemplateValue(String key) {
+        return templateData.get(key);
+    }
+
+    public boolean checkKey() {
+        return templateData.containsKey("name");
+    }
+
+    @Override
+    public String toString() {
+        return "Recipient{" +
+                "email='" + email + '\'' +
+                ", templateData=" + templateData +
+                '}';
+    }
+}
